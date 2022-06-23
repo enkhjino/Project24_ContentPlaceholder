28dc019f759f4baa29acebd6e29b2da208d51256
enkhjino <enkhjin.ochirbal1@gmail.com>
2022-06-22 20:48:14 -0700

    started javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+const header = document.getElementById('header');
+const title = document.getElementById('title');
+const excerpt = document.getElementById('excerpt');
+const profile_img = document.getElementById('profile_img');
+const name = document.getElementById('name');
+const date = document.getElementById('date');
+
+const animated_bgs = document.querySelectorAll('.animated-bg');
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
+
+function getData(){
+    header.innerHTML = '<img src="https://images.pexels.com/photos/67811/butterfly-swallowtail-papilio-animal-67811.jpeg?auto=compress&cs=tinysrgb&w=400" alt="butterfly"/>'
+    title.innerHTML = 'Lorem ipsum dolor sit amet.'
+    excerpt.innerHTML = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum veniam eius doloremque dolorum quis ad!'
+    profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="">'
+    name.innerHTML = 'John Doe'
+    date.innerHTML = 'June 22, 2022'
+}
